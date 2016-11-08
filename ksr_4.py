bdbb05e598f77dea1cb3fdc63315f226fdee0747
AndreyBadyulya <a.badyulya@gmail.com>
2016-11-08 21:41:20 +0300

    Add files via upload

--- ksr_4.py
+++ ksr_4.py
@@ -1,5 +1,11 @@
 def check_space_str(test_str):
-    return (test_str.replace('   ', ' ')).replace('   ', ' ')
+    my_list = []
+    for i in range(len(test_str)):
+        if test_str[i - 1] == ' ' and test_str[i] == ' ':
+            i += 1
+        else:
+            my_list.append(test_str[i])
+    return ''.join(my_list)
 
 if __name__ == '__main__':
-    print check_space_str('     five    four   three  two one ')
+    print check_space_str('   3               five                         four   three  two one    1')
