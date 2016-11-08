6a19380c0421de0d4ed0a34194a01a7c8406c057
AndreyBadyulya <a.badyulya@gmail.com>
2016-11-08 19:41:41 +0300

    Update ksr_1.py

--- ksr_1.py
+++ ksr_1.py
@@ -1,8 +1,8 @@
 def nat_num(num):
     if num % 10 == 0 and num % 3 == 0:
-        print 'Chislo kratno 3 i 10!'
+        return 'Chislo kratno 3 i 10!'
     else:
-        print 'Chislo ne kratno 3 i 10!'
+        return 'Chislo ne kratno 3 i 10!'
 
 if __name__ == '__main__':
     print nat_num(40)
