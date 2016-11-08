5d5f7cbceda6db9efb8f429463e7790642117e44
AndreyBadyulya <a.badyulya@gmail.com>
2016-11-08 19:47:10 +0300

    Add files via upload

---
+++ ksr_3.py
@@ -0,0 +1,12 @@
+def get_digit_sum(num):
+    list_digit = []
+    for i in range(100, num):
+        digit = i % 10 + i % 100 // 10 + i // 100
+        if digit == 7:
+            list_digit.append(i)
+            i += 1
+    return list_digit
+
+
+if __name__ == '__main__':
+    print get_digit_sum(1000)
