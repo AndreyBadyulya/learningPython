04e4a4ffdb1594a717942fef9b0a9a485e614ab8
AndreyBadyulya <a.badyulya@gmail.com>
2016-11-08 20:05:28 +0300

    Add files via upload

--- ksr_1.py
+++ ksr_1.py
@@ -1,8 +1,7 @@
 def nat_num(num):
-    if num % 10 == 0 and num % 3 == 0:
-        return 'Chislo kratno 3 i 10!'
-    else:
-        return 'Chislo ne kratno 3 i 10!'
+    return num % 10 == 0 and num % 3 == 0
+    	
+        
 
 if __name__ == '__main__':
     print nat_num(40)
