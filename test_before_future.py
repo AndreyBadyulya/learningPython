a1fc6724896d62c9aef4560d1e39c605ff004e54
AndreyBadyulya <a.badyulya@gmail.com>
2016-12-15 21:24:25 +0300

    update

--- test_before_future.py
+++ test_before_future.py
@@ -11,3 +11,13 @@ def test_1():
 @pytest.mark.usefixtures("before")
 def test_2():
     print('test_2()')
+
+@pytest.fixture
+def some_data():
+    data = {'foo': 1, 'bar': 2, 'baz': 3}
+    return data
+
+
+def test_foo(some_data):
+    print(some_data)
+    assert some_data['foo'] == 1
